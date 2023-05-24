ef08600d1c18d5da604155d96a61303b0528766a
midenceza <jose.midence14840@est.uca.edu.ni>
2023-05-24 16:54:00 -0600

    agregué los formularios

---
+++ Reporte/Reportes/Formularios/FrmVistaPrevia.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmVistaPrevia
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.BdHRDataSet = New Reporte.BdHRDataSet()
+        Me.jobsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.jobsTableAdapter = New Reporte.BdHRDataSetTableAdapters.jobsTableAdapter()
+        Me.JobsBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        CType(Me.BdHRDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.jobsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.JobsBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.JobsBindingSource1
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "Reporte.rptCargo.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(800, 450)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'BdHRDataSet
+        '
+        Me.BdHRDataSet.DataSetName = "BdHRDataSet"
+        Me.BdHRDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'jobsBindingSource
+        '
+        Me.jobsBindingSource.DataMember = "jobs"
+        Me.jobsBindingSource.DataSource = Me.BdHRDataSet
+        '
+        'jobsTableAdapter
+        '
+        Me.jobsTableAdapter.ClearBeforeFill = True
+        '
+        'JobsBindingSource1
+        '
+        Me.JobsBindingSource1.DataMember = "jobs"
+        Me.JobsBindingSource1.DataSource = Me.BdHRDataSet
+        '
+        'FrmVistaPrevia
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "FrmVistaPrevia"
+        Me.Text = "FrmVistaPrevia"
+        CType(Me.BdHRDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.jobsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.JobsBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents jobsBindingSource As BindingSource
+    Friend WithEvents BdHRDataSet As BdHRDataSet
+    Friend WithEvents jobsTableAdapter As BdHRDataSetTableAdapters.jobsTableAdapter
+    Friend WithEvents JobsBindingSource1 As BindingSource
+End Class
---
+++ Reporte/Reportes/Formularios/FrmVistaPrevia.vb
@@ -0,0 +1,23 @@
+﻿Public Class FrmVistaPrevia
+    Private Sub FrmVistaPrevia_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'BdHRDataSet.jobs' table. You can move, or remove it, as needed.
+        Me.jobsTableAdapter.Fill(Me.BdHRDataSet.jobs)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+    Sub verReportes(ByVal t As DataTable, ByVal ds As String, ByVal nombreRpt As String)
+        Try
+            Dim rpt As New ReportDataSource(ds, t)
+
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Clear()
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Add(rpt)
+            FrmVistaPrevia.ReportViewer1.LocalReport.ReportPath = nombreRpt
+            FrmVistaPrevia.ReportViewer1.Refresh()
+            FrmVistaPrevia.Show()
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al mostrar reporte")
+        End Try
+    End Sub
+End Class
